c93c6ad32b76488d801cfc05ca9e1e099402b3c3
Rita <ritaokonkwo6@gmail.com>
2020-03-07 00:15:41 +0100

    fourth commit?

--- app/src/main/java/com/project/restcountries/CountryAdapter.kt
+++ app/src/main/java/com/project/restcountries/CountryAdapter.kt
@@ -9,13 +9,17 @@ import android.widget.TextView
 import androidx.annotation.NonNull
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.project.restcountries.model.Countries
 import com.project.restcountries.model.Country2
+import java.net.URL
 import kotlin.coroutines.coroutineContext
 
-class CountryAdapter(var countryList: List<Country2>, val context: Context): RecyclerView.Adapter<CountryAdapter.ViewHolder>(){
+class CountryAdapter(var countryList: List<Country2>): RecyclerView.Adapter<CountryAdapter.ViewHolder>(){
+    lateinit var context: Context
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountryAdapter.ViewHolder {
-        val view = LayoutInflater.from(parent.context!!).inflate(R.layout.list_item, parent, false)
+        context = parent.context!!
+        val view = LayoutInflater.from(context).inflate(R.layout.list_item, parent, false)
         return ViewHolder(view)
     }
 
@@ -26,7 +30,10 @@ class CountryAdapter(var countryList: List<Country2>, val context: Context): Rec
     override fun onBindViewHolder(holder: CountryAdapter.ViewHolder, position: Int) {
         holder.name.text = countryList[position].name
         holder.capital.text = countryList[position].capital
-        Glide.with(context).load(countryList[position].flag).into(holder.flag)
+        Glide.with(holder.flag.context)
+            .load(countryList[position].flag)
+            .diskCacheStrategy(DiskCacheStrategy.NONE)
+            .into(holder.flag)
     }
 
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
--- app/src/main/java/com/project/restcountries/services/CountryService.kt
+++ app/src/main/java/com/project/restcountries/services/CountryService.kt
@@ -1,10 +1,13 @@
 package com.project.restcountries.services
 
 import com.project.restcountries.model.Country2
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+import java.util.concurrent.TimeUnit
 
 interface CountryService {
     @GET("all")
@@ -14,6 +17,14 @@ interface CountryService {
 
     companion object Factory {
         fun create(): CountryService {
+            val interceptor = HttpLoggingInterceptor()
+            interceptor.level = HttpLoggingInterceptor.Level.BODY
+            val client = OkHttpClient.Builder()
+                .connectTimeout(2, TimeUnit.MINUTES)
+                .readTimeout(2, TimeUnit.MINUTES)
+                .writeTimeout(2, TimeUnit.MINUTES)
+                .addInterceptor(interceptor)
+                .build()
             val retrofit = Retrofit.Builder()
                 .addConverterFactory(GsonConverterFactory.create())
                 .baseUrl("https://restcountries.eu/rest/v2/")
